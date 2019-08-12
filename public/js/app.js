07bad48842f73188ef6bb3843c4f1743c0115858
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-13 01:06:41 +0530

    Innit commit

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+const fetchWeatherData = function (location) {
+    const address = encodeURIComponent(location)
+    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+
+        })
+    })
+}
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('p')
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    fetchWeatherData(location)
+})
